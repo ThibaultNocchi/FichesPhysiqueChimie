6276986e25207681c3518630c4ed0d52670757e2
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-05-23 16:03:57 +0200

    Added search by name and tags

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app" class="container mt-4">
-    <FilesList></FilesList>
+    <div class="form-inline my-4">
+      <input
+        type="text"
+        id="search_query"
+        class="form-control"
+        placeholder="Recherche"
+        v-model="search_query"
+      />
+      <button class="btn btn-danger ml-2" @click="search_query = ''">
+        Effacer
+      </button>
+    </div>
+
+    <FilesList :search_query="search_query"></FilesList>
   </div>
 </template>
 
@@ -8,7 +21,12 @@
 import FilesList from "@/components/FilesList.vue";
 export default {
   name: "App",
-  components: { FilesList }
+  components: { FilesList },
+  data() {
+    return {
+      search_query: ""
+    };
+  }
 };
 </script>
 
--- src/components/FilesList.vue
+++ src/components/FilesList.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    <h2>Fichiers</h2>
-    <div class="row">
+    <h4>Fichiers</h4>
+    <div class="row" v-if="parsed_files.length !== 0">
       <div
-        v-for="(item, idx) in files"
+        v-for="(item, idx) in parsed_files"
         :key="idx"
         class="col-6 col-md-6 col-lg-4 col-xl-3 px-2 my-2"
       >
         <File :file="item"></File>
       </div>
+    </div>
+
+    <div v-else>
+      Pas de fichier correspondant à la recherche
     </div>
   </div>
 </template>
@@ -18,11 +22,26 @@ import data from "@/assets/files.json";
 import File from "@/components/File.vue";
 export default {
   name: "FilesList",
+  props: { search_query: { default: "" } },
   components: { File },
   data() {
     return {
       files: data
     };
+  },
+  computed: {
+    parsed_files() {
+      if (!this.search_query) return this.files;
+      let reg = new RegExp(this.search_query);
+      return this.files.filter(item => {
+        if (item.name.match(reg) !== null) return true;
+        let tag_found = false;
+        item.tags.forEach(tag => {
+          if (tag.match(reg)) tag_found = true;
+        });
+        return tag_found;
+      });
+    }
   }
 };
 </script>
